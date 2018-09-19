60537f763b631f49de21bac576d8ef4e17b54018
pttc@ktechbeans.com <pttc@ktechbeans.com>
2018-09-19 11:34:44 +0530

    ok

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace practice1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("saasa");
         }
     }
 }
